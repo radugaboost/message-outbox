9e27b381ba313a824cb7fb6fadb064ebb1da3941
daniil <skeal.smirnov2004@mail.ru>
2025-05-11 18:48:18 +0300

    add session maker in worker

--- message_outbox/worker.py
+++ message_outbox/worker.py
@@ -1,7 +1,7 @@
 from asyncio import gather, sleep
 
 from aiokafka import AIOKafkaProducer
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from structlog import get_logger
 
 from message_outbox.repositories import MessageOutboxRepository
@@ -11,51 +11,59 @@ logger = get_logger(__name__)
 
 
 class MessageOutboxWorker:
-    def __init__(self, session: AsyncSession, producer: AIOKafkaProducer, timeout: int = 2) -> None:
-        self.session = session
+    def __init__(
+        self,
+        session_maker: async_sessionmaker[AsyncSession],
+        producer: AIOKafkaProducer,
+        timeout: int = 2,
+    ) -> None:
+        self.session_maker = session_maker
         self.producer = producer
         self.timeout = timeout
-        self.repository = MessageOutboxRepository(session)
 
     async def process_events(self) -> None:
         logger.info("Starting event outbox worker")
+
         while True:
-            messages = await self.repository.get()
-            if not messages:
-                await sleep(self.timeout)
-                continue
+            async with self.session_maker() as session:
+                repository = MessageOutboxRepository(session)
 
-            try:
-                send_message_tasks = []
-                message_ids = []
+                messages = await repository.get()
+                if not messages:
+                    await sleep(self.timeout)
+                    continue
 
-                for message in messages:
-                    headers = [
-                        ("x-message-id", str(message.id).encode("utf-8")),
-                    ]
+                try:
+                    send_message_tasks = []
+                    message_ids = []
 
-                    if message.trace_id:
-                        headers.append(("x-trace-id", message.trace_id.encode("utf-8")))
+                    for message in messages:
+                        headers = [
+                            ("x-message-id", str(message.id).encode("utf-8")),
+                        ]
 
-                    send_message_tasks.append(
-                        self.producer.send_and_wait(
-                            topic=message.topic,
-                            value=KafkaMessageValueSchema(
-                                event_type=message.event_type, payload=message.payload
+                        if message.trace_id:
+                            headers.append(("x-trace-id", message.trace_id.encode("utf-8")))
+
+                        send_message_tasks.append(
+                            self.producer.send_and_wait(
+                                topic=message.topic,
+                                value=KafkaMessageValueSchema(
+                                    event_type=message.event_type, payload=message.payload
+                                )
+                                .model_dump_json()
+                                .encode("utf-8"),
+                                headers=headers,
                             )
-                            .model_dump_json()
-                            .encode("utf-8"),
-                            headers=headers,
                         )
-                    )
-                    message_ids.append(message.id)
+                        message_ids.append(message.id)
 
-                await gather(*send_message_tasks)
+                    await gather(*send_message_tasks)
 
-                await self.repository.batch_mark_processed(message_ids)
-                await self.session.commit()
+                    await repository.batch_mark_processed(message_ids)
+                    await session.commit()
 
-                logger.info("Successfully sent messages", count=len(message_ids))
-            except Exception as exc:
-                await self.session.rollback()
-                raise exc
+                    logger.info("Successfully sent messages", count=len(message_ids))
+                except Exception as exc:
+                    await session.rollback()
+                    raise exc
